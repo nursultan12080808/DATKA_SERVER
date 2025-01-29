942cd69cdeac4996f3205b535f11ac85e877270f
nursultan12080808 <nursultan.top.game@gmail.com>
2025-01-29 06:15:23 +0600

    commit

--- datka_project/settings.py
+++ datka_project/settings.py
@@ -77,7 +77,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'oshai4683@gmail.com'
+EMAIL_HOST_USER = 'nursultan.top.game@gmail.com'
 EMAIL_HOST_PASSWORD = 'ecozopexqbxyettp'
 
 
